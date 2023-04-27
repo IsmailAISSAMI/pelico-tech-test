7128a678b6ff0102af14b012060eb67ebca45c65
Aissami <aissami.ismail@gmail.com>
2023-04-27 08:51:28 -0400

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,7 @@ import App from './App';
 
 test('renders the online store title', () => {
   render(<App />);
-  const titleElement = screen.getByText(/My Online Store/i);
+  const titleElement = screen.getByText(/Web store application/i);
   expect(titleElement).toBeInTheDocument();
 });
 
