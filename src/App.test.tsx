a32eb0e367baa4218ea495aea05fddcb1d3a5fca
Aissami <aissami.ismail@gmail.com>
2023-04-27 08:56:21 -0400

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,2 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
 
-test('renders the online store title', () => {
-  render(<App />);
-  const titleElement = screen.getByText(/Web store application/i);
-  expect(titleElement).toBeInTheDocument();
-});
 
